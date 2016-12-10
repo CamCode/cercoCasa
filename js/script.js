d21aa61b84c6b162f251bb839a6fa322b319d59d
camila <camifer55@gmail.com>
2016-12-09 22:44:37 -0500

    menu fixed al hacer scroll

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@
 
     $(".player").mb_YTPlayer();
 
+
+    //menu fixed scroll 
+
+	var nav = $('#nav-cerco');
+   $(window).scroll(function () {
+    //muestra menu al hacer scroll
+        if ($(this).scrollTop() > 148) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }    
+        
+    
+    });
+
+
 });
